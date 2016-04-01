bfe5175bb6f1359a4463389790cb4924dc6f9d60
carmela121 <mcarmencarmona@hotmail.com>
2016-04-01 16:09:21 +0100

    fixing routes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/api', router);
+app.use('/', router);
 
 app.listen(port, function() {
   console.log("Express is listening on port " + port);
