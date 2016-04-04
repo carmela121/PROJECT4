b47fba195e99a5ffeb199e433cfe082a9dc0f3ea
carmela121 <mcarmencarmona@hotmail.com>
2016-04-04 11:54:18 +0100

    making car form

--- models/car.js
+++ models/car.js
@@ -5,7 +5,8 @@ var carSchema = mongoose.Schema({
   make:  { type: String, required: true },
   model: { type: String, required: true },
   price: { type: String, required: true },
-  image: { type: String, required: true }
+  image: { type: String, required: true },
+  miles: { type: String, required: true }
 });
 
 module.exports = mongoose.model("Car", carSchema);
