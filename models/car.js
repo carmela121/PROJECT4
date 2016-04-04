df51cd5755a95557ef9ec25d3c3683a2f450d5df
carmela121 <mcarmencarmona@hotmail.com>
2016-04-04 17:51:52 +0100

    added a booking form

--- models/car.js
+++ models/car.js
@@ -1,13 +1,16 @@
 var mongoose = require('mongoose');
 
 var carSchema = mongoose.Schema({
+
   year:  { type: String, required: true },
   make:  { type: String, required: true },
   model: { type: String, required: true },
   price: { type: String, required: true },
   image: { type: String, required: true },
   miles: { type: String, required: true },
-  postCode: { type: String, required: true }
+  postCode: { type: String, required: true },
+  availability: { type: String, required: true }
+
 });
 
 module.exports = mongoose.model("Car", carSchema);
