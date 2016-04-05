679e2f7fb185ffb669fbe8a02736fbd8e4fdc485
carmela121 <mcarmencarmona@hotmail.com>
2016-04-05 13:44:36 +0100

    setting up add car functionality

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,10 +1,24 @@
 var Car = require('../models/car');
+var User = require('../models/user');
 
 
 function carsIndex(req, res) {
   Car.find(function(err, cars) {
     if(err) return res.status(500).json({ message: err });
     return res.status(200).json(cars);
+  });
+}
+function carsCreate(req, res){
+  var car = new Car(req.body.car);
+  car.save(function(err){
+    if (err) return res.status(500).send(err);
+    var name = req.body.car.user;
+    User.findOne({ name: name }, function(err, user){
+      user.cars.push(car);
+      user.save(function(err, user) {
+        res.status(201).send(car);
+      });
+    });
   });
 }
 
@@ -14,6 +28,8 @@ function carsShow(req, res) {
     return res.status(200).json(car);
   });
 }
+
+
 
 function carsUpdate(req, res) {
   Car.findByIdAndUpdate(req.params.id, req.body, { new: true }, function(err, car) {
@@ -31,6 +47,7 @@ function carsDelete(req, res) {
 
 module.exports = {
   index: carsIndex,
+  create: carsCreate,
   show: carsShow,
   update: carsUpdate,
   delete: carsDelete
